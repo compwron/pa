d0f4d4bcd586168895ccc2217e1daf59167b0b01
Martin DeMello <mdemello@google.com>
2015-11-17 21:06:50 -0800

    refactor and add tests

--- lib/penny_allocation.rb
+++ lib/penny_allocation.rb
@@ -1,34 +1,39 @@
+require 'ostruct'
+
 module PennyAllocation
   def reallocate_partial_pennies(array_of_values, options = {})
-    fractional_sum = 0
-
-    array_of_hashes = array_of_values.map {|v| { value: v }}
-
-    values = array_of_hashes.sort_by do |hash|
-      fractional_value = hash[:value] % 1
-      hash[:value] = hash[:value].floor
-      fractional_sum += fractional_value
-      1 - fractional_value
+    # split into whole and fractional parts.
+    # preserve the original position of each value, since we will be sorting
+    # the array and need to unsort at the end.
+    values = array_of_values.each_with_index.map do |v, i|
+      OpenStruct.new.tap do |val|
+        val.whole, val.fractional = v.divmod 1
+        val.index = i
+      end
     end
 
-    number_to_allocate = if options[:comp_total]
-                           round_comp_total(fractional_sum)
-                         else
-                           fractional_sum.round
-                         end
+    # round off the sum, since we can't end with a fraction of a penny
+    partial_pennies = values.inject(0) {|sum, v| sum + v.fractional}
+    partial_pennies = round_total(options, partial_pennies)
 
-    values.each_with_index do |hash, index|
-      hash[:value] += 1 if index < number_to_allocate
+    # round down the values, and redistribute the total partial_pennies among
+    # them in decreasing order of their original fractional pennies
+    values = values.sort_by {|v| -v.fractional}
+    (0 ... partial_pennies).each do |i|
+      values[i].whole += 1
     end
 
-    array_of_hashes.map {|h| h[:value] }
+    # restore the original order and return the adjusted whole penny values
+    values.sort_by(&:index).map(&:whole)
   end
 
-  def round_comp_total(number)
-    if number % 1 == 0.500000
-      number.floor
+  # round the total fractional pennies
+  # if options[:comp_total] is set, 0.5 is rounded downards.
+  def round_total(options, total)
+    if options[:comp_total] and total % 1 == 0.5
+      total.floor
     else
-      number.round
+      total.round
     end
   end
 end
