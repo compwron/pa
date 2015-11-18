ffaf63c299e5e35ebdd299f8ca52e4a927a3c5b3
compwron <compiledwrong@gmail.com>
2015-11-18 00:10:11 -0700

    Amazing refactor by @martindemello

--- lib/penny_allocation.rb
+++ lib/penny_allocation.rb
@@ -1,54 +1,39 @@
-require 'pry'
+require 'ostruct'
+
 module PennyAllocation
-  def reallocate_partial_pennies(values, options = {})
-    number_to_allocate = number_to_allocate(options, fractional_sum(values))
-
-    floored_values_with_fractional_remainders(values).sort_by do |hash|
-      1 - hash[:fractional_value]
-    end.each_with_index.map do |hash, index|
-      if index < number_to_allocate
-        hash.merge({value: hash[:value] + 1})
-      else
-        hash
+  def reallocate_partial_pennies(array_of_values, options = {})
+    # split into whole and fractional parts.
+    # preserve the original position of each value, since we will be sorting
+    # the array and need to unsort at the end.
+    values = array_of_values.each_with_index.map do |v, i|
+      OpenStruct.new.tap do |val|
+        val.whole, val.fractional = v.divmod 1
+        val.index = i
       end
-    end.sort_by do |hash|
-      hash[:index]
-    end.map do |hash|
-      hash[:value]
     end
+
+    # round off the sum, since we can't end with a fraction of a penny
+    partial_pennies = values.inject(0) {|sum, v| sum + v.fractional}
+    partial_pennies = round_total(options, partial_pennies)
+
+    # round down the values, and redistribute the total partial_pennies among
+    # them in decreasing order of their original fractional pennies
+    values = values.sort_by {|v| -v.fractional}
+    (0 ... partial_pennies).each do |i|
+      values[i].whole += 1
+    end
+
+    # restore the original order and return the adjusted whole penny values
+    values.sort_by(&:index).map(&:whole)
   end
 
-  def round_comp_total(number)
-    if fractional_value(number) == 0.500000
-      number.floor
+  # round the total fractional pennies
+  # if options[:comp_total] is set, 0.5 is rounded downwards.
+  def round_total(options, total)
+    if options[:comp_total] and total % 1 == 0.5
+      total.floor
     else
-      number.round
-    end
-  end
-
-  private
-
-  def fractional_sum(values)
-    values.map do |val|
-      fractional_value(val)
-    end.inject(&:+) || 0
-  end
-
-  def floored_values_with_fractional_remainders(values)
-    values.each_with_index.map do |v, original_position|
-      {fractional_value: fractional_value(v), value: v.floor, index: original_position}
-    end
-  end
-
-  def fractional_value(value)
-    value % 1
-  end
-
-  def number_to_allocate(options, fractional_sum)
-    if options[:comp_total]
-        round_comp_total(fractional_sum)
-    else
-      fractional_sum.round
+      total.round
     end
   end
 end
